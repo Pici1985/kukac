50c359c4457acf9d8f7ac864e4bf1aac5fed4bdc
Balazs <balazs@frog.net>
2022-11-18 07:05:31 +0000

    maybe works

--- app.js
+++ app.js
@@ -1,36 +1,55 @@
 
-let days = 174;
+let days = 88;
 let felnottkukac = 2;
 let gyerekkukac = 0; 
 
 function calculate(days, felnottkukac, gyerekkukac){
-    if(days <= 87){       
-        let generaciokszama = Math.floor(days / 3); 
+    
+    let hanyadikfelnoves = Math.ceil(days / 87);
+    
+    console.log(hanyadikfelnoves);
 
-        gyerekkukac = generaciokszama * 2.7;
+    let napiszorzo = Math.ceil((days - 87) / 3);
+    
+    console.log(napiszorzo);
+    
+    let generaciokszama = Math.floor(days / 3); 
 
-        console.log(`A ${days}. napon a felnott kukacok szama ${felnottkukac}, a gyerekkukacok szama ${gyerekkukac}!`)
+    if(days <= 87 ){
+        felnottkukac = 2;
+        gyerekkukac = (generaciokszama * 2.7)
+    } else {
+        gyerekkukac = (( generaciokszama * 2.7 ) * napiszorzo ) * hanyadikfelnoves;
+        felnottkukac = felnottkukac + ((napiszorzo * 2) * hanyadikfelnoves); 
+    } 
 
-    } else if(87 < days && days <= 174){
-        let generaciokszama = Math.floor(days / 3);    
-
-        let szorzo = Math.floor((days - 87) / 3);
-
-        console.log(szorzo);
-
-        if(szorzo === 0){
-            gyerekkukac = generaciokszama * 2.7 
-            felnottkukac = felnottkukac
-        } else {
-            gyerekkukac = Math.floor(generaciokszama * 2.7);
-            felnottkukac += (szorzo * 2);  
-        }            
-
-        console.log(`A ${days}. napon a felnott kukacok szama ${felnottkukac}, a gyerekkukacok szama ${gyerekkukac}!`)
-
-    } else if(174 < days && days < 261){
-        console.log("anyad");
-    }
+    console.log(`A ${days}. napon a felnott kukacok szama ${felnottkukac}, a gyerekkukacok szama ${gyerekkukac}!`);
 }
+    
+    // if(i <= 87){       
+
+
+    //     console.log(`A ${days}. napon a felnott kukacok szama ${felnottkukac}, a gyerekkukacok szama ${gyerekkukac}!`)
+    // }    
+
+    // } else if(87 < days && days <= 174){
+    //     let generaciokszama = Math.floor(days / 3);    
+
+
+    //     console.log(szorzo);
+
+    //     if(szorzo === 0){
+    //         gyerekkukac = generaciokszama * 2.7 
+    //         felnottkukac = felnottkukac
+    //     } else {
+    //         gyerekkukac = Math.floor(generaciokszama * 2.7);
+    //         felnottkukac += (szorzo * 2);  
+    //     }            
+
+    //     console.log(`A ${days}. napon a felnott kukacok szama ${felnottkukac}, a gyerekkukacok szama ${gyerekkukac}!`)
+
+    // } else if(174 < days && days < 261){
+    //     console.log("anyad");
+    // }
 
 calculate(days, felnottkukac, gyerekkukac);
